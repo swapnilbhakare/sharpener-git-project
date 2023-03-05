8a75c090ba87413e4491699a8950cd0414082511
swapnilbhakare <swapnilbhakare7@gmail.com>
2023-03-05 18:11:42 +0530

    practice on document objects

--- app.js
+++ app.js
@@ -1,2 +1,32 @@
-console.log("I love git");
-const app = "computer";
+// Examine the document object
+// console.dir(document)
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document);
+// console.log(document.title);
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.all);
+// console.log(document.all[10]);
+// document.all[10].textContent = "Hello";
+// console.log(document.forms);
+// console.log(document.links);
+// console.log(document.images);
+
+// Get Element by ID
+
+const headerTitle = document.getElementById("header-title");
+const header = document.getElementById("main-header");
+
+// headerTitle.textContent = "Hello";
+// headerTitle.innerText = "Goodbye";
+// console.log(headerTitle.innerText);
+// headerTitle.innerHTML = "<h3>Hello</h3>";
+// header.style.borderBottom = "solid 3px #000";
+// Get Elements by class name
+const items = document.getElementsByClassName("list-group-item");
+console.log(items);
+// console.log(items[1]);
+items[1].textContent = "Hello 2";
+items[1].style.fontWeight = "bold";
+items[1].style.color = "green";
---
+++ git-add.app.js
@@ -0,0 +1,6 @@
+//
+// DOM - Document opject model
+
+// Tree of nodes/elements created by browser
+// JavaScript can be used to read/write/manipulate to the DOM
+// Object Oriented Representation
