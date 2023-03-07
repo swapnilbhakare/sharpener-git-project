296d55f058c6e786f07ea584a6b493e92ca8a820
swapnilbhakare <swapnilbhakare7@gmail.com>
2023-03-07 10:07:18 +0530

    new element added without same class and changed getelement by class to tag

--- app.js
+++ app.js
@@ -24,13 +24,24 @@ const header = document.getElementById("main-header");
 // headerTitle.innerHTML = "<h3>Hello</h3>";
 // header.style.borderBottom = "solid 3px #000";
 // Get Elements by class name
-const items = document.getElementsByClassName("list-group-item");
-console.log(items);
+// const items = document.getElementsByClassName("list-group-item");
+// console.log(items);
 // console.log(items[1]);
 // items[1].textContent = "Hello 2";
 // items[1].style.fontWeight = "bold";
 // items[1].style.color = "green";
-for (let i = 0; i < items.length; i++) {
-  items[i].style.fontWeight = "bold";
-  items[i].style.backgroundColor = "green";
+// for (let i = 0; i < items.length; i++) {
+//   items[i].style.fontWeight = "bold";
+//   items[i].style.backgroundColor = "green";
+// }
+
+const li = document.getElementsByTagName("li");
+console.log(li);
+// console.log(li[1]);
+// li[1].textContent = "Hello 2";
+// li[1].style.fontWeight = "bold";
+// li[1].style.color = "green";
+for (let i = 0; i < li.length; i++) {
+  li[i].style.fontWeight = "bold";
+  li[i].style.backgroundColor = "green";
 }
