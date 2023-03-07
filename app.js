034052984d4d26f2eb158b322fed727509b5e9fc
swapnilbhakare <swapnilbhakare7@gmail.com>
2023-03-07 10:01:02 +0530

    all lists background make green and font weight  bold

--- app.js
+++ app.js
@@ -27,6 +27,10 @@ const header = document.getElementById("main-header");
 const items = document.getElementsByClassName("list-group-item");
 console.log(items);
 // console.log(items[1]);
-items[1].textContent = "Hello 2";
-items[1].style.fontWeight = "bold";
-items[1].style.color = "green";
+// items[1].textContent = "Hello 2";
+// items[1].style.fontWeight = "bold";
+// items[1].style.color = "green";
+for (let i = 0; i < items.length; i++) {
+  items[i].style.fontWeight = "bold";
+  items[i].style.backgroundColor = "green";
+}
